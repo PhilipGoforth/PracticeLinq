907116325e3bbae48f7618e1932152204232d623
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-12 11:47:01 -0600

    final

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameGames = new List<string>() {"Halo", "Halo 2", "Skyrim", "World of Warcraft" };
+
+            nameGames.OrderByDescending(x => x.Length);
+            nameGames.ForEach(x => Console.WriteLine(x));
+            
+        }
+    }
+}
